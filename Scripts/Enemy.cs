d7e7fb265fee70092374ea78f145c1264ea764eb
ringoQED <38661475+ringoQED@users.noreply.github.com>
2021-10-17 20:30:18 +0800

    Add files via upload

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -7,9 +7,9 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
 
-    public Transform Player;
+    public Transform Player, WeaponCam;
     public float MoveSpeed = 2.0f;
-    public float MaxDist = 20.0f;
+    public float MaxDist = 50.0f;
     public float MinDist = 10.0f;
     public AudioClip zombie_roar;
     public AudioClip zombie_hurt;
@@ -25,6 +25,11 @@ public class Enemy : MonoBehaviour
     public float health = 100.0f;
     private bool dead = false;
 
+    public GameObject fb, fb_clone;
+    public float PushForce = 500.0f;
+    public bool Busy = false;
+    public float WaitTime = 1.0f;
+
     void Start()
     {
         Player = GameObject.FindWithTag("Player").transform;
@@ -33,6 +38,11 @@ public class Enemy : MonoBehaviour
         NM = GetComponent<NavMeshAgent>();
         NM.enabled = false;
         health = 100.0f;
+
+        fb = GameObject.Find("Fireball");
+        Player = GameObject.Find("Player").transform;
+        WeaponCam = GameObject.Find("WeaponCam").transform;
+
     }
 
     void Update()
@@ -40,26 +50,30 @@ public class Enemy : MonoBehaviour
 
         Dist = Vector3.Distance(transform.position, Player.position); //calculate the distance between the player and the zombie
 
-        //Debug.Log("Distance = "+ Dist);
-        Debug.Log("Enemy health : " + health);
-
         Ray eyeSight = new Ray(transform.position, Player.position);
-        //int layerMask = LayerMask.GetMask("Player");
 
         if (Physics.Raycast(eyeSight, out RaycastHit hit, hitDist))
-        //if (Physics.Raycast(eyeSight, out RaycastHit hit, hitDist, layerMask))
         {
 
             if (hit.collider.CompareTag("Player"))   //Player seen by zombie
             {
+
                 inSight = true;
-                Debug.Log("Player seen! inSight is " + inSight);
-            } 
+
+            }
 
         }
 
         if (inSight)    //Player seen by zombie
         {
+
+            transform.LookAt(WeaponCam.position);   // Make zombie to  look at player's direction
+
+            if (!Busy)
+            {
+                StartCoroutine("Eject");
+            }
+
 
             if ((Dist > MinDist) && (Dist <= MaxDist))
             {                
@@ -103,7 +117,25 @@ public class Enemy : MonoBehaviour
             //change to idle
             anim.SetTrigger("Idle");            
         }
+
     }
+
+    IEnumerator Eject()
+    {
+        Busy = true;
+
+        fb_clone = Instantiate(fb, GameObject.Find("FBSpawnPoint").transform.position, GameObject.Find("FBSpawnPoint").transform.rotation) as GameObject;
+
+        fb_clone.transform.LookAt(WeaponCam.position);
+
+        fb_clone.GetComponent<Rigidbody>().AddForce(fb_clone.transform.forward * PushForce);
+
+        yield return new WaitForSeconds(WaitTime);
+
+        Busy = false;
+    }
+
+
 
     internal void Damage(float dmg)
     {
@@ -122,9 +154,10 @@ public class Enemy : MonoBehaviour
                 NM.enabled = false;
                 this.enabled = false;
 
+                Destroy(fb);    // Destroy the Fireball game object to stop fireball action
+
             }
 
-            Debug.Log("Enemy health : " + health);
         }
         
         
